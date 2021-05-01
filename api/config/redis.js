5d517a671ae8ed90a28c5b28a905763c800a1d48
Arpan <arpnagar1@gmail.com>
2021-05-01 09:28:56 +0530

    again

--- api/config/redis.js
+++ api/config/redis.js
@@ -1,8 +1,10 @@
 import redis from 'redis';
 import connectRedis from 'connect-redis';
 
-const client = redis.createClient(6379, process.env.REDIS_HOST || 'localhost');
-
+const client = redis.createClient(11886, process.env.REDIS_HOST, {
+    auth_pass: 'HAKiWDcby1WkQjFJ8g8wKhqSepi5xvKU',
+    return_buffers: true,
+});
 const redisConfig = {
     client: client,
     ttl: 604801,
